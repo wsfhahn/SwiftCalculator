1ba26685ca07ef2a11b587d6ec4d595b5133de1d
wsfhahn <wsfhahn@icloud.com>
2021-04-07 09:07:35 -0400

    Wrote code

--- SwiftCalculator/main.swift
+++ SwiftCalculator/main.swift
@@ -7,5 +7,69 @@
 
 import Foundation
 
-print("Hello, World!")
+func gatherData() -> (Float, Float, String) {
+    var num1: Float
+    var num2: Float
+    var oper: String
+    
+    print("First number?")
+    num1 = Float(readLine()!)!
+    
+    print("Second number?")
+    num2 = Float(readLine()!)!
+    
+    print("Operator?")
+    oper = String(readLine()!)
+    
+    return (num1, num2, oper)
+}
 
+func add(num1: Float, num2: Float) -> Float {
+    var result: Float
+    result = num1 + num2
+    return result
+}
+
+func subtract(num1: Float, num2: Float) -> Float {
+    var result: Float
+    result = num1 - num2
+    return result
+}
+
+func multiply(num1: Float, num2: Float) -> Float {
+    var result: Float
+    result = num1 * num2
+    return result
+}
+
+func divide(num1: Float, num2: Float) -> Float {
+    var result: Float
+    result = num1 / num2
+    return result
+}
+
+let userData = gatherData()
+let num1 = userData.0
+let num2 = userData.1
+let oper = userData.2
+
+func doMath(num1: Float, num2: Float, oper: String) {
+    var result: Float = 0
+    
+    switch oper {
+    case "+":
+        result = add(num1: num1, num2: num2)
+    case "-":
+        result = subtract(num1: num1, num2: num2)
+    case "*":
+        result = multiply(num1: num1, num2: num2)
+    case "/":
+        result = divide(num1: num1, num2: num2)
+    default:
+        print("Error calculating answer")
+    }
+    
+    print("\(num1) \(oper) \(num2) = \(result)")
+}
+
+doMath(num1: num1, num2: num2, oper: oper)
